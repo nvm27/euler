da64c398e782b777ce50c5d4611907390bc451ef
Kamil Wojtuch <kamil@straylight.im>
2015-02-04 21:37:27 +0100

    Solution to third problem: Largest prime factor.

---
+++ src/largest_prime.rs
@@ -0,0 +1,12 @@
+use primes::PrimeSet;
+use std::num::Float;
+
+pub fn result(number: u64) -> u64 {
+    let max: u64 = Float::sqrt(number as f64) as u64 + 1;
+
+    PrimeSet::new().iter()
+        .take_while(|&x| x <= max)
+        .filter(|&x| number % x == 0)
+        .max()
+        .unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+extern crate primes;
+
 mod multiples;
 mod even_fib;
+mod largest_prime;
 
 fn main() {
-    println!("Multiples of 3 and 5: {}", multiples::result(1000));
-    println!("Even Fibonacci numbers: {}", even_fib::result(4000000));
+    println!("Multiples of 3 and 5: {}", multiples::result(1_000));
+    println!("Even Fibonacci numbers: {}", even_fib::result(4_000_000));
+    println!("Largest prime factor: {}", largest_prime::result(600_851_475_143));
 }
