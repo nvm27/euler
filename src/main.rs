82f611e6ccd34980312d34df57852e4a59736a54
Kamil Wojtuch <kamil@straylight.im>
2015-02-08 19:35:40 +0100

    Smallest multiple

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+#![feature(core)]
+
 extern crate primes;
 
 mod multiples;
 mod even_fib;
 mod largest_prime;
 mod palindrome;
+mod smallest_multiple;
 
 fn main() {
     println!("Multiples of 3 and 5: {}", multiples::result(1_000));
     println!("Even Fibonacci numbers: {}", even_fib::result(4_000_000));
     println!("Largest prime factor: {}", largest_prime::result(600_851_475_143));
     println!("Largest palindrome product: {}", palindrome::result(100, 1000));
+    println!("Smallest multiple: {}", smallest_multiple::result(20));
 }
---
+++ src/smallest_multiple.rs
@@ -0,0 +1,14 @@
+use primes::PrimeSet;
+use std::iter;
+
+pub fn result(max: u64) -> u64 {
+    PrimeSet::new().iter()
+        .take_while(|&x| x <= max)
+        .map(
+            |x| iter::iterate(1, |y| x*y)
+                .take_while(|&y| y <= max)
+                .last()
+                .unwrap()
+        )
+        .fold(1, |product, x| product * x)
+}
